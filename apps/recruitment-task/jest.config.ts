fee47bec96a076b2a2a3f6973d8a2b053492ec32
Wojtek <wojtekgg93@gmail.com>
2024-11-16 20:36:02 +0100

    Initial commit

---
+++ apps/recruitment-task/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'recruitment-task',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/recruitment-task',
+};
---
+++ apps/recruitment-task/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="recruitment-task" />
+    </div>
+  );
+}
+
+export default App;
